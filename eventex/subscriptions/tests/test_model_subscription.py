aa360f94569ca356c952fd9c55612758e0c80b90
Eric Vieira <eric.vieira@me.com>
2019-07-16 10:42:21 -0300

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from django.test import TestCase
+
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+
+    def setUp(self):
+        self.obj = Subscription(
+            name='Eric Vieira',
+            cpf='12345678901',
+            email='eric.vieira@me.com',
+            phone='21-99889-3320'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+
+    def test_created_at(self):
+        """Subscription must have an a auto created_at attr. """
+        self.assertIsInstance(self.obj.created_at, datetime)
